4e229378223e62613d1ed8fd424a4211610042d8
Salzmannn <114645578+Salzmannn@users.noreply.github.com>
2023-10-27 10:38:15 +0200

    created database locally & created table structure

---
+++ datenbank_pythonORM/create_tables.sql
@@ -0,0 +1,74 @@
+CREATE TABLE "radios" (
+  "id" integer PRIMARY KEY,
+  "name" text,
+  "status_id" integer,
+  "currently_playing" text,
+  "stream_url" text,
+  "logo_url" text
+);
+
+CREATE TABLE "radio_states" (
+  "id" integer PRIMARY KEY,
+  "label" text
+);
+
+CREATE TABLE "radio_genres" (
+  "radio_id" integer,
+  "genre_id" integer
+);
+
+CREATE TABLE "genres" (
+  "id" integer PRIMARY KEY,
+  "name" text
+);
+
+CREATE TABLE "connections" (
+  "id" integer PRIMARY KEY,
+  "search_query" text,
+  "search_without_ads" bool,
+  "search_remaining_update" integer,
+  "preference_music" bool,
+  "preference_talk" bool,
+  "preference_news" bool,
+  "preference_ad" bool
+);
+
+CREATE TABLE "connection_preferred_radios" (
+  "radio_id" integer,
+  "connection_id" integer
+);
+
+CREATE TABLE "connection_preferred_genres" (
+  "connection_id" integer,
+  "genre_id" integer
+);
+
+CREATE TABLE "connection_search_favorites" (
+  "radio_id" integer,
+  "connection_id" integer
+);
+
+CREATE TABLE "radio_ad_time" (
+  "radio_id" integer,
+  "ad_start_time" timestamp
+);
+
+ALTER TABLE "radios" ADD FOREIGN KEY ("status_id") REFERENCES "radio_states" ("id");
+
+ALTER TABLE "radio_genres" ADD FOREIGN KEY ("radio_id") REFERENCES "radios" ("id");
+
+ALTER TABLE "radio_genres" ADD FOREIGN KEY ("genre_id") REFERENCES "genres" ("id");
+
+ALTER TABLE "connection_preferred_radios" ADD FOREIGN KEY ("radio_id") REFERENCES "radios" ("id");
+
+ALTER TABLE "connection_preferred_radios" ADD FOREIGN KEY ("connection_id") REFERENCES "connections" ("id");
+
+ALTER TABLE "connection_preferred_genres" ADD FOREIGN KEY ("connection_id") REFERENCES "connections" ("id");
+
+ALTER TABLE "connection_preferred_genres" ADD FOREIGN KEY ("genre_id") REFERENCES "genres" ("id");
+
+ALTER TABLE "connection_search_favorites" ADD FOREIGN KEY ("radio_id") REFERENCES "radios" ("id");
+
+ALTER TABLE "connection_search_favorites" ADD FOREIGN KEY ("connection_id") REFERENCES "connections" ("id");
+
+ALTER TABLE "radio_ad_time" ADD FOREIGN KEY ("radio_id") REFERENCES "radios" ("id");
