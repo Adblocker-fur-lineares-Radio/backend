7c3f380976acc2d08efe7e6349bd99808d93a841
kaany <kaan.yazici@alumni.fh-aachen.de>
2023-10-29 12:05:56 +0100

    Client v3 None Tested

--- main.py
+++ main.py
@@ -1,53 +1,90 @@
 import vlc
+import json
 import time
 import asyncio
+
+import websockets
 from websockets.sync.client import connect
 
-def hello():
-    with connect("ws://localhost:8765") as websocket:
-        websocket.send("Hello world!")
-        message = websocket.recv()
-        print(f"Received: {message}")
+
+def CheckPollingUpdate(ws):
+    check = ws.recv()
+    if check:
+        return check
+    else:
+        return 0
 
 
+async def NeededResponse(ws):
+    response_json = await ws.recv()
+    return response_json
 
 
-def play():
-    url = 'https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3?aggregator=radio-de'
-    url2 = 'https://wdr-wdr2-rheinruhr.icecastssl.wdr.de/wdr/wdr2/rheinruhr/mp3/56/stream.mp3?aggregator=surfmusik-de&1697723004'
+async def Connect2Server():
+    async with websockets.connect("ws://localhost:1234") as ws:
+        return ws
+
+
+def search_request(ws, query, filter_ids=None, filter_without_ads=False, requested_updates=1):
+    ws.send(json.dumps({
+        'type': 'search_request',
+        'query': query,
+        'requested_updates': requested_updates,
+        'filter': {
+            'ids': filter_ids,
+            'without_ads': filter_without_ads
+        }
+    }))
+
+
+def search_update_request(ws, requested_updates=1):
+    ws.send(json.dumps({
+        'type': 'search_update_request',
+        'requested_updates': requested_updates
+    }))
+
+
+def stream_request(ws, preferred_radios=None, preferred_genres=None, preferred_experience=None):
+    ws.send(json.dumps({
+        'type': 'stream_request',
+        'preferred_radios': preferred_radios or [],
+        'preferred_genres': preferred_genres or [],
+        'preferred_experience': preferred_experience or {'ad': False, 'news': True, 'music': True}
+    }))
+
+
+def StartClient():
+    ws = asyncio.run((Connect2Server()))
+    stream_request(ws)
+    response = NeededResponse(ws)
+    data = json.load(open(response))
+
+    if data["type"] == "stream_guidance":
+        url = data["radio"]
+        buffer = data["buffer"]
+    else:
+        print("Error 404")
+        return
+
+    current_stream = url
+
+    # url1 = 'https://wdr-wdr2-rheinruhr.icecastssl.wdr.de/wdr/wdr2/rheinruhr/mp3/56/stream.mp3?aggregator=surfmusik-de&1697723004'
+    # url2 = 'https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3?aggregator=radio-de'
 
     instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
-    instance2 = vlc.Instance('--input-repeat=-1', '--fullscreen')
-
     player = instance.media_player_new()
-    player2 = instance2.media_player_new()
-
-    media = instance.media_new(url)
-    media2 = instance2.media_new(url2)
-
+    media = instance.media_new(current_stream)
     player.set_media(media)
-    player2.set_media(media2)
-
     player.play()
-    aktellerPlayer = player
-
-
 
     while 1:
-        if time.strftime("%H:%M:%S", time.localtime()) >= "11:28:00":
-            if aktellerPlayer == player:
-                player.stop()
-                aktellerPlayer = player2
-                time.sleep(2)
-                player2.play()
-            else:
-                player2.stop()
-                aktellerPlayer = player
-                time.sleep(2)
-                player.play()
+        check = CheckNeededUpdate(ws)
+        if check == 0:
+            print("loop")
+        else:
+            print("Change")
+            # do something
 
 
 if __name__ == '__main__':
-    play()
-
-
+    StartClient()
