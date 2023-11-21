39f9f8023b29f0d4f12c20a031b45a3208063fc2
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2023-11-21 09:57:35 +0100

    fixed error that came from error handling

--- api/stream_request.py
+++ api/stream_request.py
@@ -68,7 +68,6 @@ def stream_request(client, connection_id, req):
     update_preferences_for_connection(
         connection_id,
         preferred_radios=req["preferred_radios"],
-        preferred_genres=req["preferred_genres"],
         preference_ad=req["preferred_experience"]["ad"],
         preference_talk=req["preferred_experience"]["talk"],
         preference_music=req["preferred_experience"]["music"],
