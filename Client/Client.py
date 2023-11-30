6d9b300463635f40a6d731fb57a1b8b6bda50058
Kaan <115877679+abc4561@users.noreply.github.com>
2023-11-30 15:46:22 +0100

    Finger + ServerRecord

--- Client/Client.py
+++ Client/Client.py
@@ -3,16 +3,24 @@ import vlc
 import json
 import asyncio
 import websockets
+from dejavu.recognize import FileRecognizer
+
 # from api.db.database_functions import delete_connection_from_db
 
 address = "ws://185.233.107.253:5000/api"
+from dejavu import Dejavu
+import time
+import sys
+from urllib.request import urlopen
+
+
 # address = "ws://127.0.0.1:5000/api"
 
 
 def search_request(requested_updates=1):
     """
     Look up ServerAPI Documentation (Google Drive)
-    @param query: Contains Search-text
+    @param query: Contains Search-textS
     @param filter_ids: Contains preferred Radios
     @param filter_without_ads: bool, if you want ads or not
     @param requested_updates: Number of follow-up updates expected
@@ -117,5 +125,111 @@ async def StartClient():
                 pass
 
 
+
+def test():
+    config = {
+        "database": {
+            "host": "localhost",
+            "user": "test123",
+            "password": "test123",
+            "database": "finger"
+        },
+    }
+    djv = Dejavu(config)
+    djv.fingerprint_directory(r"C:\Users\kaany\Desktop\geschnitten", [".wav"])
+    print(djv.db.get_num_fingerprints())
+
+    # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\WerbungTest.mp3"))
+    # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\NoneTest.mp3"))
+    # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\Orginal\1live_news.mp3"))
+    # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\Orginal\1live_werbung.mp3"))
+    # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\Orginal\bayern1_news.mp3"))
+    # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\Orginal\bayern1_werbung.mp3"))
+    # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\Orginal\wdr2_news.mp3"))
+    # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\Orginal\wdr2_werbung.mp3"))
+    # return
+
+    response = urlopen("https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3?aggregator=radio-de",
+                       timeout=10.0)
+
+    fname1 = "1_" + str(time.perf_counter())[2:] + ".wav"
+    f1 = open(fname1, 'wb')
+
+    fname2 = "2_" + str(time.perf_counter())[2:] + ".wav"
+    f2 = open(fname2, 'wb')
+
+    fname3 = "3_" + str(time.perf_counter())[2:] + ".wav"
+    f3 = open(fname3, 'wb')
+
+    start = time.time()
+    offset = 2
+    dauer = 10
+
+    while True:  # wird einmalig ausgeführt
+        try:
+            audio = response.read(1024)
+
+            if start + dauer - offset < time.time() < start + 2 * dauer - offset:  # 2. Block first offset Part
+                f2.write(audio)
+                sys.stdout.flush()
+
+            if time.time() <= start + dauer:  # 1. Block
+                f1.write(audio)
+                sys.stdout.flush()
+            else:
+                break
+
+        except Exception as e:
+            print("Error " + str(e))
+
+    f1.close()
+    sys.stdout.flush()
+    print(djv.recognize(FileRecognizer, fname1))
+    i = 4
+
+    while True:  # wird dauerhaft ausgeführt
+        start = time.time()
+        try:
+            while time.time() - start < dauer - offset:
+                audio = response.read(1024)
+                if start < time.time() < start + dauer - offset:
+                    f2.write(audio)
+                    sys.stdout.flush()
+                if start + dauer - 2 * offset < time.time() < start + dauer - offset:
+                    f3.write(audio)
+                    sys.stdout.flush()
+
+            f2.close()
+            sys.stdout.flush()
+            print(djv.recognize(FileRecognizer, fname2))
+            fname2 = str(i) + "_" + str(time.perf_counter())[2:] + ".wav"
+            f2 = open(fname2, 'wb')
+            i += 1
+
+            while time.time() - start < 2 * dauer - offset:
+                audio = response.read(1024)
+                if start + dauer < time.time() < start + 2 * dauer - offset:
+                    f3.write(audio)
+                    sys.stdout.flush()
+                if start + 2 * dauer - 2 * offset < time.time() < start + 2 * dauer - offset:
+                    f2.write(audio)
+                    sys.stdout.flush()
+
+            f3.close()
+            sys.stdout.flush()
+            print(djv.recognize(FileRecognizer, fname3))
+            fname3 = str(i) + "_" + str(time.perf_counter())[2:] + ".wav"
+            f3 = open(fname3, 'wb')
+            i += 1
+
+        except Exception as e:
+            print("Error " + str(e))
+
+
 if __name__ == '__main__':
-    asyncio.run(StartClient())
+    # asyncio.run(StartClient())
+    test()
+
+    # pip install git+https://github.com/yunpengn/dejavu.git
+    # pip install mysql
+    # ffmpeg: https://phoenixnap.com/kb/ffmpeg-windows
