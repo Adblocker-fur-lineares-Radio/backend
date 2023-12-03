efc079bcd22f8d79dfcde9e7bee8df3c57566bdf
Kaan <115877679+abc4561@users.noreply.github.com>
2023-12-03 13:36:26 +0100

    Audiorecord improved

--- Client/Client.py
+++ Client/Client.py
@@ -1,19 +1,14 @@
-import time
 import vlc
 import json
 import asyncio
 import websockets
 from dejavu.recognize import FileRecognizer
-
-# from api.db.database_functions import delete_connection_from_db
-
-address = "ws://185.233.107.253:5000/api"
 from dejavu import Dejavu
 import time
 import sys
 from urllib.request import urlopen
 
-
+address = "ws://185.233.107.253:5000/api"
 # address = "ws://127.0.0.1:5000/api"
 
 
@@ -147,7 +142,6 @@ def test():
     # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\Orginal\bayern1_werbung.mp3"))
     # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\Orginal\wdr2_news.mp3"))
     # print(djv.recognize(FileRecognizer, r"C:\Users\kaany\Desktop\Orginal\wdr2_werbung.mp3"))
-    # return
 
     response = urlopen("https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3?aggregator=radio-de",
                        timeout=10.0)
@@ -162,7 +156,7 @@ def test():
     f3 = open(fname3, 'wb')
 
     start = time.time()
-    offset = 2
+    offset = 3
     dauer = 10
 
     while True:  # wird einmalig ausgeführt
@@ -182,10 +176,27 @@ def test():
         except Exception as e:
             print("Error " + str(e))
 
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
     f1.close()
-    sys.stdout.flush()
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
     print(djv.recognize(FileRecognizer, fname1))
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
     i = 4
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
+    f2.write(response.read(1024))
+    sys.stdout.flush()
 
     while True:  # wird dauerhaft ausgeführt
         start = time.time()
@@ -199,12 +210,41 @@ def test():
                     f3.write(audio)
                     sys.stdout.flush()
 
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
             f2.close()
-            sys.stdout.flush()
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
             print(djv.recognize(FileRecognizer, fname2))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            sys.stdout.flush()
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
             fname2 = str(i) + "_" + str(time.perf_counter())[2:] + ".wav"
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
             f2 = open(fname2, 'wb')
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
             i += 1
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            f3.write(response.read(1024))
+            sys.stdout.flush()
 
             while time.time() - start < 2 * dauer - offset:
                 audio = response.read(1024)
@@ -215,12 +255,41 @@ def test():
                     f2.write(audio)
                     sys.stdout.flush()
 
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
             f3.close()
-            sys.stdout.flush()
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
             print(djv.recognize(FileRecognizer, fname3))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            sys.stdout.flush()
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
             fname3 = str(i) + "_" + str(time.perf_counter())[2:] + ".wav"
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
             f3 = open(fname3, 'wb')
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
             i += 1
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            f2.write(response.read(1024))
+            sys.stdout.flush()
 
         except Exception as e:
             print("Error " + str(e))
