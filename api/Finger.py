b486fd5abd45ec93a83084c17b455368988cdf83
Kaan <115877679+abc4561@users.noreply.github.com>
2023-12-08 15:02:06 +0100

    Testing Threads

--- api/Finger.py
+++ api/Finger.py
@@ -5,6 +5,7 @@ import sys
 from urllib.request import urlopen
 import os
 import threading
+from datetime import datetime
 
 
 def test(Url, Offset, Dauer , FingerThreshold):
@@ -51,64 +52,89 @@ def test(Url, Offset, Dauer , FingerThreshold):
     sys.stdout.flush()
     f1.close()
 
-    finger = djv.recognize(FileRecognizer, fname1)
-    if finger and finger["confidence"] > fingerThreshold:
-        print(finger)
+    if os.stat(fname1).st_size > 0:
+        finger = djv.recognize(FileRecognizer, fname1)
+        if finger and finger["confidence"] > fingerThreshold:
+            print(datetime.now().strftime("%H:%M:%S") + ": " + str(finger))
+
     os.remove(fname1)
 
     i = 4
     while True:
         start = time.time()
-        try:
-            while time.time() - start <= dauer - offset:
-                audio = response.read(1024)
-                f2.write(audio)
-                sys.stdout.flush()
-
-                if start + dauer - 2 * offset < time.time():
-                    f3.write(audio)
-                    sys.stdout.flush()
-
+        #try:
+        while time.time() - start <= dauer - offset:
+            audio = response.read(1024)
+            f2.write(audio)
             sys.stdout.flush()
-            f2.close()
 
-            finger2 = djv.recognize(FileRecognizer, fname2)
-            if finger2 and finger2["confidence"] > fingerThreshold:
-                print(finger2)
-
-            os.remove(fname2)
-            fname2 = str(i) + "_" + str(time.perf_counter())[2:] + ".wav"
-            f2 = open(fname2, 'wb')
-            sys.stdout.flush()
-            i += 1
-
-            while time.time() - start <= 2 * dauer - offset:
-                audio = response.read(1024)
+            if start + dauer - 2 * offset < time.time():
                 f3.write(audio)
                 sys.stdout.flush()
 
-                if start + 2 * dauer - 2 * offset < time.time():
-                    f2.write(audio)
-                    sys.stdout.flush()
+        f2.close()
+        #sys.stdout.flush()
 
+        if os.stat(fname2).st_size > 0:
+            finger2 = djv.recognize(FileRecognizer, fname2)
+            if finger2 and finger2["confidence"] > fingerThreshold:
+                print(datetime.now().strftime("%H:%M:%S") + ": " + str(finger2))
+
+
+        # os.remove(fname2)
+        # fname2 = str(i) + "_" + str(time.perf_counter())[2:] + ".wav"
+        f2 = open(fname2, 'wb')
+        sys.stdout.flush()
+        i += 1
+
+        while time.time() - start <= 2 * dauer - offset:
+            audio = response.read(1024)
+            f3.write(audio)
             sys.stdout.flush()
-            f3.close()
+
+            if start + 2 * dauer - 2 * offset < time.time():
+                f2.write(audio)
+                sys.stdout.flush()
+
+
+        f3.close()
+        #sys.stdout.flush()
+
+        if os.stat(fname3).st_size > 0:
             finger3 = djv.recognize(FileRecognizer, fname3)
             if finger3 and finger3["confidence"] > fingerThreshold:
-                print(finger3)
+                print(datetime.now().strftime("%H:%M:%S") + ": " + str(finger3))
 
-            os.remove(fname3)
-            fname3 = str(i) + "_" + str(time.perf_counter())[2:] + ".wav"
-            f3 = open(fname3, 'wb')
-            sys.stdout.flush()
-            i += 1
 
-        except Exception as e:
-            print("Error " + str(e))
+        # os.remove(fname3)
+        # fname3 = str(i) + "_" + str(time.perf_counter())[2:] + ".wav"
+        f3 = open(fname3, 'wb')
+        sys.stdout.flush()
+        i += 1
+
+        #except Exception as e:
+            #print("Error " + str(e))
+
+def p():
+    config = {
+        "database": {
+            "host": "localhost",
+            "user": "test123",
+            "password": "test123",
+            "database": "finger"
+        },
+    }
+    djv = Dejavu(config)
+
+    if os.stat("2_14.7695461.wav").st_size > 0:
+        finger = djv.recognize(FileRecognizer, "2_14.7695461.wav")
+        if finger and finger["confidence"] > 0:
+            print(datetime.now().strftime("%H:%M:%S") + ": " + str(finger))
 
 
 if __name__ == '__main__':
     #test("https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3?aggregator=radio-de", 2, 10, 10)
+    # p()
     x = threading.Thread(target=test, args=("https://wdr-1live-live.icecastssl.wdr.de/wdr/1live/live/mp3/128/stream.mp3?aggregator=radio-de", 2, 10, 10))
     x.start()
 
