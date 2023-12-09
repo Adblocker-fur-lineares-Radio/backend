e4e226c4ac7ac9a77279e4504ecb303f7144b4bd
Chris <chris.henkes@alumni.fh-aachen.de>
2023-12-09 14:07:53 +0100

    metadata title fix

--- api/db/database_functions.py
+++ api/db/database_functions.py
@@ -614,7 +614,7 @@ def get_radios_and_update_by_currently_playing(data):
             currently_playing = title.split(' - ')
 
         if len(currently_playing) == 1:
-            currently_playing.append(None)
+            currently_playing.append('')
 
         stmt = (select(Radios)
                 .where(Radios.currently_playing != currently_playing[1])
