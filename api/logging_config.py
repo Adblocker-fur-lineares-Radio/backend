359569424d50fd6f9ddd3d237c84e7990605ff25
Salzmannn <114645578+Salzmannn@users.noreply.github.com>
2023-12-18 15:31:50 +0100

    logging metadata in logs/metadata.csv now

--- api/logging_config.py
+++ api/logging_config.py
@@ -1,7 +1,7 @@
 import logging
 import os
-
-
+import csv
+from datetime import datetime
 
 
 def configure_logging():
@@ -16,4 +16,36 @@ def configure_logging():
                             logging.FileHandler(log_file_path),
                             logging.StreamHandler()
                         ]
-                    )
+                        )
+
+
+configure_logging()
+logger = logging.getLogger("logging_config.py")
+
+
+def csv_logging_setup():
+    logs_directory = os.path.abspath(os.path.join(os.getcwd(), 'logs'))
+    os.makedirs(logs_directory, exist_ok=True)
+
+
+def csv_logging_write(data):
+    filename = '/logs/metadata.csv'
+    header = ['date', 'stationID', 'interpret', 'title']
+    rowdata = data
+    currenttime = datetime.now()
+    formattedtime = currenttime.strftime("%m/%d/%Y %I:%M:%S %p")
+    rowdata.insert(0, formattedtime)
+
+    if data is not None:
+        try:
+
+            with open(filename, 'a', newline='') as csv_file:
+                csv_writer = csv.writer(csv_file, delimiter=';')
+                if csv_file.tell() == 0:
+                    csv_writer.writerow(header)
+                csv_writer.writerow(data)
+                csv_file.flush()
+        except Exception as e:
+            logger.error("ERROR IN WRITE")
+            raise e
+    return
