067ab83fc3a29310c71d9009f9fd5bc742ee3e57
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2024-01-04 14:14:37 +0100

    update script now faster (excluded .git directory)

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 del v1.zip
-"C:\Program Files\7-Zip\7z.exe" a "v1.zip" ".\*"
+"C:\Program Files\7-Zip\7z.exe" a "v1.zip" ".\*" -xr!.git -xr!logs
 scp v1.zip root@v2202311209121242520.luckysrv.de:/root/
 ssh root@v2202311209121242520.luckysrv.de './update_dev.sh'
 
