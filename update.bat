69a54d049b95f635cff1a0e8da274f4c9a02d150
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2024-02-13 21:48:44 +0100

    removed pycache from upload script

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 del v1.zip
-"C:\Program Files\7-Zip\7z.exe" a "v1.zip" ".\*" -xr!.git -xr!logs
+"C:\Program Files\7-Zip\7z.exe" a "v1.zip" ".\*" -xr!.git -xr!logs -xr!__pycache__
 scp v1.zip root@v2202311209121242520.luckysrv.de:/root/
 ssh root@v2202311209121242520.luckysrv.de './update_stable.sh'
 
