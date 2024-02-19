922224a8cb8dab6c61bffd8ed662b100c8a8b259
Chris Henkes <chris.henkes@alumni.fh-aachen.de>
2024-02-19 18:38:46 +0100

    Delete update.bat

--- update.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-del v1.zip
-"C:\Program Files\7-Zip\7z.exe" a "v1.zip" ".\*" -xr!.git -xr!logs -xr!__pycache__
-scp v1.zip root@v2202311209121242520.luckysrv.de:/root/
-ssh root@v2202311209121242520.luckysrv.de './update_stable.sh'
-
-
